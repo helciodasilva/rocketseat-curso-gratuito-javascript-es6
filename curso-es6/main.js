d38bfe5314328cb78f3bcc8d821c812db338f449
Helcio da Silva <helciodasilva@gmail.com>
2018-11-02 20:46:39 -0300

    Aula 5

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,9 +1,11 @@
-class Matematica {
+function teste(x) {
+    let y = 2;
 
-    static soma(a, b) {
-        return a + b;
+    if (x > 5) {
+        let y = 4;
+
+        console.log(x, y);
     }
-
 }
 
-console.log(Matematica.soma(1, 2));
+teste(10);
