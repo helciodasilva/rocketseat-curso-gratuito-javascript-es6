7ea7c03e8ed1ab8191cc26337443615e3a15dca7
Helcio da Silva <helciodasilva@gmail.com>
2018-11-02 22:18:21 -0300

    Aula 11

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,19 +1,5 @@
-// REST
+const nome = "Diego";
+const idade = 23;
 
-function soma(a, b, ...params) {
-    return params;
-}
-
-console.log(soma(1, 2, 3, 4, 5, 6, 7));
-
-// SPREAD
-
-const usuario1 = {
-    nome: 'Diego',
-    idade: 23,
-    empresa: 'Rocketseat'
-}
-
-const usuario2 = { ...usuario1, nome: 'Gabriel'};
-
-console.log(usuario2);
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
