255b1ca41b5172b78e78af9cb548d4deb521610d
Helcio da Silva <helciodasilva@gmail.com>
2018-11-02 22:21:42 -0300

    Aula 12

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,5 +1,10 @@
-const nome = "Diego";
+const nome = 'Diego';
 const idade = 23;
 
-console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
-console.log(`Meu nome é ${nome} e tenho ${idade} anos`);
+const usuario = {
+    nome,
+    idade,
+    empresa: 'Rocketseat'
+};
+
+console.log(usuario);
