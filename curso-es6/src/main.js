e64c27cc5705dd4134dffd272d5aae3a5a625fd7
Helcio da Silva <helciodasilva@gmail.com>
2018-11-04 13:42:27 -0200

    Aula 19

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,14 +1,28 @@
-import axios from 'axios';
+class App {
+	constructor() {
+		this.repositories = [];
+		this.formEl = document.getElementById('repo-form');
 
-class Api {
-    static async getUserInfo(username) {
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);            
-            console.log(response);
-        } catch (err) {
-            console.warn('Erro na API');
-        }
-    }
+		this.registerListeners();
+	}
+	
+	registerListeners() {
+		this.formEl.onsubmit = event => 
+		this.addRepository(event);
+	}
+	
+	addRepository(event) {
+		event.preventDefault();
+		
+		this.repositories.push({
+			avatar_url: 'https://avatars0.githubusercontent.com/u/28929274',
+			name: 'rocketseat.com.br',
+			description: 'Tire sua ideia do papel e dê vida à sua startup',
+			html_url: 'https://github.com/RocketSeat/rocketseat.com.br',
+		});
+		
+		console.log(this.repositories);
+	}
 }
 
-Api.getUserInfo('diego3g');
+new App();
