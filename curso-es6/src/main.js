08e3fee48eaaa71a8559229e8437be5629259aeb
Helcio da Silva <helciodasilva@gmail.com>
2018-11-04 12:13:49 -0200

    Aula 16

--- curso-es6/src/main.js
+++ curso-es6/src/main.js
@@ -1,1 +1,20 @@
-alert('Testando');
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK') }, 2000);
+});
+
+// minhaPromise().then(response => ... );
+
+//async function executaPromise() {
+//    console.log(await minhaPromisse());
+//    console.log(await minhaPromisse());
+//    console.log(await minhaPromisse());
+//}
+
+const executaPromise = async () => {
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+};
+
+executaPromise();
+
